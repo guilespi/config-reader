7ac3b8da772668ab12e6599da7b4f9df96abcea3
Guillermo Winkler <guillermo.winkler@gmail.com>
2014-09-30 14:53:57 -0300

    fix readme and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject config-reader "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Reads configuration options from file or environment "
+  :url "https://github.com/guilespi/config-reader"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
